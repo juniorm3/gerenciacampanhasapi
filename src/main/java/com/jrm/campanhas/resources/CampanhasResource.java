d2d9bae9bae262d21e6524be165d888fc7b2cf4a
Mario Junior <juniorm3@msn.com>
2018-01-13 18:44:06 -0200

    Criando tratamento para respostas HTTP de forma adequada

--- src/main/java/com/jrm/campanhas/resources/CampanhasResource.java
+++ src/main/java/com/jrm/campanhas/resources/CampanhasResource.java
@@ -1,13 +1,18 @@
 package com.jrm.campanhas.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.jrm.campanhas.domain.Campanha;
 import com.jrm.campanhas.repository.CampanhasRepository;
@@ -20,28 +25,47 @@ public class CampanhasResource {
 	private CampanhasRepository campanhasRepository;
 
 	@RequestMapping(method = RequestMethod.GET)
-	public List<Campanha> listar() {
-		return campanhasRepository.findAll();
+	public ResponseEntity<List<Campanha>> listar() {
+		return ResponseEntity.status(HttpStatus.OK).body(campanhasRepository.findAll());
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public void salvar(@RequestBody Campanha campanha) {
-		campanhasRepository.save(campanha);
+	public ResponseEntity<Void> salvar(@RequestBody Campanha campanha) {
+		campanha = campanhasRepository.save(campanha);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(campanha.getId())
+				.toUri();
+
+		return ResponseEntity.created(uri).build();
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public Campanha buscar(@PathVariable("id") Long id) {
-		return campanhasRepository.findOne(id);
+	public ResponseEntity<?> buscar(@PathVariable("id") Long id) {
+		Campanha campanha = campanhasRepository.findOne(id);
+
+		if (campanha == null) {
+			return ResponseEntity.notFound().build();
+		}
+
+		return ResponseEntity.status(HttpStatus.OK).body(campanha);
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-	public void deletar(@PathVariable("id") Long id) {
-		campanhasRepository.delete(id);
+	public ResponseEntity<Void> deletar(@PathVariable("id") Long id) {
+		try {
+			campanhasRepository.delete(id);
+		} catch (EmptyResultDataAccessException e) {
+			return ResponseEntity.notFound().build();
+		}
+
+		return ResponseEntity.noContent().build();
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-	public void atualizar(@RequestBody Campanha campanha, @PathVariable("id") Long id) {
+	public ResponseEntity<Void> atualizar(@RequestBody Campanha campanha, @PathVariable("id") Long id) {
 		campanha.setId(id);
 		campanhasRepository.save(campanha);
+		
+		return ResponseEntity.noContent().build();
 	}
 }
