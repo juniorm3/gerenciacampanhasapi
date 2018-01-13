b97558c92f64a7be371c683d18dd7e649f887a3f
Mario Junior <juniorm3@msn.com>
2018-01-13 15:49:49 -0200

    Criação de projeto para gerenciamento de campanhas

---
+++ src/main/java/com/jrm/campanhas/GerenciacampanhasapiApplication.java
@@ -0,0 +1,12 @@
+package com.jrm.campanhas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciacampanhasapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciacampanhasapiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/jrm/campanhas/GerenciacampanhasapiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jrm.campanhas;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GerenciacampanhasapiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
